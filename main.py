8ae2625e82a306ace184859b2ee0a1532c7e86f7
Santi Urbina <urbinasanti1@gmail.com>
2022-05-03 09:31:20 -0600

    Archivo Python
    Este es el archivo de Python en donde se muestran mis datos.

---
+++ main.py
@@ -0,0 +1,7 @@
+# Datos de mi.
+
+print("Mi nombre es Angel Santiago Urbina Lopez")
+print("Mi carnet es 1546122")
+print("Mi carrera es Ingenieria en informatica y sistemas")
+print("Actualmente estoy en el primer semestre")
+print("Estoy cursando la carrera en la Universidad Rafael Landivar")

fa48e6c655380b92f07caf765ce7350a31ecbce1
Santi Urbina <urbinasanti1@gmail.com>
2022-05-03 09:36:58 -0600

    Ingreso de animal favorito
    Esta es la actualizacion donde se ingresa el animal favorito del usuario en el programa Python.

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ print("Mi carnet es 1546122")
 print("Mi carrera es Ingenieria en informatica y sistemas")
 print("Actualmente estoy en el primer semestre")
 print("Estoy cursando la carrera en la Universidad Rafael Landivar")
+
+# Mi animal favorito
+
+animal = input("Ingrese su animal favorito: ")
+
+if animal == "Pantera negra":
+    print("A mi tambien me gusta mucho las panteras negras")
+else:
+    print("Es un animal interesante, pero a mi me gustan mas las panteras negras.")
